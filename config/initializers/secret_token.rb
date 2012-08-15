d1bb19b624113fda5c043c306b4a78e26bbf4095
calveto <todd.calvert@gmail.com>
2012-08-14 20:33:27 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LetsCook::Application.config.secret_token = 'b38ea2d58a83b892514b34ea86c9d5676abe79dbd5c561a6baca5452089d7b6c27979b677d9f37a4008403e1929a9fffa1dd76618ac762739f26b6cbd5a2d883'
